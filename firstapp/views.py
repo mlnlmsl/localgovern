20c80cd1c3c3b3e0fa9092fc4777eaa7470cd3ff
Tekson <teksondada@gmail.com>
2019-07-21 01:16:01 +0545

    Title of the webpage changes

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,18 +4,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 
+import pyrebase
+config = {
+  "apiKey": "AIzaSyAbLSZ6wFGRloWQQCka348uSZtZdojDqFw",
+  "authDomain": "loca-9518f.firebaseapp.com",
+  "databaseURL": "https://loca-9518f.firebaseio.com",
+  "projectId": "loca-9518f",
+  "storageBucket": "",
+  "messagingSenderId": "1054814527011",
+  "appId": "1:1054814527011:web:4ed004d9390b738e"
+}
 
-# config = {
-#   "apiKey": "AIzaSyAbLSZ6wFGRloWQQCka348uSZtZdojDqFw",
-#   "authDomain": "loca-9518f.firebaseapp.com",
-#   "databaseURL": "https://loca-9518f.firebaseio.com",
-#   "projectId": "loca-9518f",
-#   "storageBucket": "",
-#   "messagingSenderId": "1054814527011",
-#   "appId": "1:1054814527011:web:4ed004d9390b738e"
-# }
-
-# firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+data = db.child('')
 
 # auth = firebase.auth()
 def signin(request):
